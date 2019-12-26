32b301e7213fdfbae277457c10e4f1d5db2812b7
Jared <jaredlll08@gmail.com>
2019-12-26 17:00:53 +0200

    no more formik, no more yup, redid register and working on login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import {NextPage} from 'next';
-import {Button, Form} from 'react-bootstrap';
-import {Formik} from 'formik';
-import * as yup from 'yup';
+import {useState} from "react";
+import {Alert, Button, Col, Container, Form} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import {SyntheticEvent} from "react";
 import {API_URL} from "../utils/api";
+import Link from "next/link";
 
-const schema = yup.object().shape({
-  username: yup.string().required(),
-  password: yup.string().required(),
-  remember: yup.bool().required(),
-  mfa: yup.string(),
-});
 
-function login(data: object) {
-  // @ts-ignore
-  const {username, password, mfa} = data;
-
+function login(event: SyntheticEvent, username: string, password: string, setErrors: Function) {
+  event.preventDefault();
+  console.log(event.target);
   const formData = new URLSearchParams();
   formData.append('username', username);
   formData.append('password', password);
-  formData.append('mfa', mfa);
+  // formData.append('mfa', mfa);
 
   fetch(`${API_URL}/v1/auth/login`, {
     method: "post",
@@ -32,93 +26,85 @@ function login(data: object) {
     body: formData
   })
     .then((response) => {
-      console.log(response);
+      if (response.status == 200) {
+        setErrors([]);
+        return;
+      }
+      response.json().then(data => {
+        setErrors(data["message"]);
+      })
     });
 }
 
-let showMFA = false;
-const RegisterPage: NextPage = () => (
-  <Layout title="Login | Diluv">
-    <div className="container">
-      <div className="row">
-        <div className="col-sm">
+
+function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [errors, setErrors] = useState([]);
+
+  return (<Layout title="Register | Diluv">
+      <div className="container">
+        <div className="pb-md-2 pt-md-3 text-center">
+          <h1>Login</h1>
         </div>
-        <div className="col-sm">
-          <div className="pb-md-5 text-center">
-            <h1>Login</h1>
-          </div>
-          <Formik
-            validationSchema={schema}
-            onSubmit={login}
-            initialValues={{
-              username: '',
-              password: '',
-              mfa: '',
-              remember: false
-            }}>
-            {({
-                handleSubmit,
-                handleChange,
-                values,
-                touched,
-                errors,
-              }) => (
-              <Form noValidate onSubmit={handleSubmit}>
-                <Form.Group controlId="usernameId" hidden={showMFA}>
+        <Container>
+          {errors.length != 0 &&
+          <Row className={"justify-content-md-center"}>
+            <Col md={4}>
+              <Alert variant={"danger"}>
+                {errors}
+              </Alert>
+            </Col>
+          </Row>
+          }
+          <Form onSubmit={(e: SyntheticEvent) => login(e, username, password, setErrors)}>
+            <Form.Row className={"justify-content-md-center"}>
+              <Col md={4}>
+                <Form.Group controlId="usernameId" className={"mb-1"}>
                   <Form.Label>Username</Form.Label>
                   <Form.Control
-                    type="text"
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                    isValid={touched.username && !errors.username}
+                    type={"username"}
+                    name={"username"}
+                    defaultValue={""}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+                    minLength={3}
+                    maxLength={50}
+
                   />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                 </Form.Group>
-                <Form.Group controlId="passwordId" hidden={showMFA}>
+              </Col>
+            </Form.Row>
+            <Form.Row className={"justify-content-md-center"}>
+              <Col md={4}>
+                <Form.Group controlId="passwordId" className={"mb-1"}>
                   <Form.Label>Password</Form.Label>
                   <Form.Control
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    isValid={touched.password && !errors.password}
-                  />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group hidden={showMFA}>
-                  <Form.Check
-                    required
-                    name="remember"
-                    label="Remember Me"
-                    isInvalid={!!errors.remember}
-                    value={`${values.remember}`}
-                    feedback={errors.remember}
-                    onChange={handleChange}
-                    id="terms"
+                    type={"password"}
+                    name={"pasword"}
+                    defaultValue={""}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                    minLength={8}
+                    maxLength={70}
                   />
                 </Form.Group>
-                <Form.Group controlId="mfaId" hidden={!showMFA}>
-                  <Form.Label>MultiFactor</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="mfa"
-                    value={values.mfa}
-                    onChange={handleChange}
-                    isValid={touched.mfa && !errors.mfa}
-                  />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Button type="submit">Submit form</Button>
-              </Form>
-            )}
-          </Formik>
-        </div>
-        <div className="col-sm">
-        </div>
+              </Col>
+            </Form.Row>
+            <Form.Row className={"justify-content-md-center pt-2"}>
+              <Col md={4}>
+                <Button type="submit" disabled={username.trim().length < 3 && password.trim().length < 8} block>Login</Button>
+              </Col>
+            </Form.Row>
+          </Form>
+          <Row className={"justify-content-md-center pt-2"}>
+            <Col md={4}>
+              <p className={"text-center"}>Don't have an account? <Link href={"/register"}>Register now!</Link></p>
+            </Col>
+          </Row>
+        </Container>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+}
 
 export default RegisterPage
