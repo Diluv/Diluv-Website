7fe11b9c3fef581204df81cdc45a4cb5b307c8fa
Jared <jaredlll08@gmail.com>
2020-01-06 00:03:32 +0200

    Started work on light / dark theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,33 @@
+export function getTheme(): 'light' | 'dark' {
+  if (typeof localStorage === "undefined") {
+    return "light";
+  }
+  let item = localStorage.getItem("theme");
+  if (!item) {
+    item = "light";
+    localStorage.setItem("theme", item);
+  }
+  // @ts-ignore
+  return item;
+}
+
+export function setTheme(theme: string) {
+  if (typeof localStorage === "undefined") {
+    return;
+  }
+  localStorage.setItem("theme", theme);
+}
+
+export const toggleTheme = function (): 'light' | 'dark' {
+  if (typeof localStorage === "undefined") {
+    return "light";
+  }
+  let item = localStorage.getItem("theme");
+  if (!item) {
+    item = "light";
+  }
+  let inverse = item === "light" ? "dark" : "light";
+  localStorage.setItem("theme", inverse);
+  // @ts-ignore
+  return inverse;
+};
