86ca79b2ae230760bdecd9eefca0053d689b6df2
Jared <jaredlll08@gmail.com>
2020-01-06 22:23:37 +0200

    made footer make more sense component wise

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -103,53 +103,55 @@ const Layout: React.FunctionComponent<Props> = ({
     <div className={"theme-" + theme}>
       {children}
     </div>
-    <footer className={"pt-4 py-md-5 pt-md-5 border-top container theme-" + theme}>
-      <div className="row">
-        <div className="col-12 col-md">
-          <img className="mb-2" src={diluv} alt="" width="24" height="24"/>
-          <small className="d-block mb-3 text-muted">© 2019</small>
-        </div>
-        <div className="col-6 col-md">
-          <h5>Product</h5>
-          <ul className="list-unstyled text-small">
-            <li><a href="/news">News</a></li>
-            {
-              false && (
-                <li><a href="https://ideas.diluv.com">Feedback</a></li>
-              )
-            }
-            {
-              false && (
-                <li><a href="https://blog.diluv.com">Blog</a></li>
-              )
-            }
-            {
-              false && (
-                <li><a href="https://developer.diluv.com">Developers</a></li>
-              )
-            }
-            <li><a href="https://github.com/Diluv">Github</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md">
-          <h5>About</h5>
-          <ul className="list-unstyled text-small">
-            <li><a href="/about">About</a></li>
-            <li><a href="/privacy">Privacy</a></li>
-            <li><a href="/terms">Terms</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md">
-          <h5>Contact</h5>
-          <ul className="list-unstyled text-small">
-            <li><a href="https://twitter.com/DiluvSupport">Twitter</a></li>
-            <li><a href="https://www.reddit.com/r/diluv/">Reddit</a></li>
-            {
-              false && (
-                <li><a href="#">Discord</a></li>
-              )
-            }
-          </ul>
+    <footer className={"pt-4 py-md-5 pt-md-5 border-top theme-" + theme}>
+      <div className={"container"}>
+        <div className="row">
+          <div className="col-12 col-md">
+            <img className="mb-2" src={diluv} alt="" width="24" height="24"/>
+            <small className="d-block mb-3 text-muted">© 2019</small>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Product</h5>
+            <ul className="list-unstyled text-small">
+              <li><a href="/news">News</a></li>
+              {
+                false && (
+                  <li><a href="https://ideas.diluv.com">Feedback</a></li>
+                )
+              }
+              {
+                false && (
+                  <li><a href="https://blog.diluv.com">Blog</a></li>
+                )
+              }
+              {
+                false && (
+                  <li><a href="https://developer.diluv.com">Developers</a></li>
+                )
+              }
+              <li><a href="https://github.com/Diluv">Github</a></li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>About</h5>
+            <ul className="list-unstyled text-small">
+              <li><a href="/about">About</a></li>
+              <li><a href="/privacy">Privacy</a></li>
+              <li><a href="/terms">Terms</a></li>
+            </ul>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Contact</h5>
+            <ul className="list-unstyled text-small">
+              <li><a href="https://twitter.com/DiluvSupport">Twitter</a></li>
+              <li><a href="https://www.reddit.com/r/diluv/">Reddit</a></li>
+              {
+                false && (
+                  <li><a href="#">Discord</a></li>
+                )
+              }
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
