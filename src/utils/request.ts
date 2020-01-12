01759be872280cd06a934b029061a12a67b062e7
Jared <jaredlll08@gmail.com>
2020-01-12 14:02:50 +0200

    refresh token on requests

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,16 +1,69 @@
 import axios from 'axios';
-
+import {destroyCookie, parseCookies, setCookie} from 'nookies';
+import jwt from 'jwt-decode'
+import {API_URL} from "./api";
 
 export function post(url: string, data: any, headers: any = {
   'Accept': 'application/json'
 }) {
-  if(process.browser) {
-    if (!headers.Authorization && localStorage && localStorage.getItem("accessToken")) {
-      headers.Authorization = "Bearer " + localStorage.getItem("accessToken")
+  if (process.browser) {
+    let token = parseCookies(null)["accessToken"];
+    if (!headers.Authorization && token) {
+      let jwtToken = jwt<{ exp: number }>(token);
+      let current_time = new Date().getTime() / 1000;
+      if (current_time > jwtToken.exp) {
+        refresh(); // Should put new things into the cookie
+        token = parseCookies(null)["accessToken"];
+        if (token) {
+          headers.Authorization = "Bearer " + token
+        }
+      } else {
+        headers.Authorization = "Bearer " + token
+      }
     }
   }
   return axios.post(url, data, {
     headers: headers
+  })
+}
+
+export function refresh() {
+  let accessToken = parseCookies(null)["accessToken"];
+  if (!accessToken) {
+    return;
+  }
+  let jwtToken = jwt<{ exp: number }>(accessToken);
+  let current_time = new Date().getTime() / 1000;
+  if (current_time < jwtToken.exp) {
+    return;
+  }
+  let refreshToken = parseCookies(null)["refreshToken"];
+  if (!refreshToken) {
+    return;
+  }
+
+  axios.post(API_URL + "/v1/auth/refresh", {}, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': "Bearer " + refreshToken
+    }
+  }).then(value => {
+    setCookie(null, "accessToken", value.data.data.accessToken, {
+      expires: new Date(value.data.data.expiredAt)
+    });
+    setCookie(null, "username", jwt<{ username: string }>(value.data.data.accessToken)["username"], {
+      expires: new Date(value.data.data.expiredAt)
+    });
+    setCookie(null, "refreshToken", value.data.data.refreshToken, {
+      expires: new Date(value.data.data.refreshExpiredAt)
+    });
+  }).catch(() => {
+    // If refresh failed, essentially log user out
+    destroyCookie(null, "accessToken");
+    destroyCookie(null, "username");
+    destroyCookie(null, "refreshToken");
+
   })
 }
 
