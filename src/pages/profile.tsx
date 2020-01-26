b57eef8b821e82e4e9b870c7ff7b3a90094adb53
lclc98 <lclc98@lclc98.com>
2020-01-26 22:36:44 +1000

    Added avatarURL and createdAt

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,12 +5,13 @@ import {ensureAuthed} from "../utils/auth";
 import {useEffect, useRef, useState} from "react";
 import {get} from "../utils/request";
 import {API_URL} from "../utils/api";
-import {parseCookies} from "nookies";
 import {AxiosError} from "axios";
 
 
 interface UserInfo {
-  username: string,
+  username: string
+  avatarURL: string
+  createdAt: number
   email: string
   mfa: boolean
 }
@@ -19,13 +20,18 @@ interface UserInfo {
 function ProfilePage() {
 
   ensureAuthed(null, "/login?rt=/profile");
-  const userInfo = useRef<UserInfo>({email: "loading...", mfa: false, username: "loading..."});
+  const userInfo = useRef<UserInfo>({
+    email: "loading...",
+    mfa: false,
+    username: "loading...",
+    avatarURL: 'https://www.gravatar.com/avatar/00000000000000000000000000000000',
+    createdAt: 0
+  });
 
   const [, forceUpdate] = useState({});
 
   useEffect(() => {
-    let username = parseCookies()["username"];
-    get(API_URL + "/v1/users/" + username).then((value) => {
+    get(API_URL + "/v1/users/me").then((value) => {
       userInfo.current = value.data.data;
       forceUpdate({});
     }).catch((Error: AxiosError) => {
@@ -36,18 +42,18 @@ function ProfilePage() {
   if (!userInfo.current) {
     return <div></div>;
   }
-  return (<Layout title="Profile | Diluv" >
+  return (<Layout title="Profile | Diluv">
       <div className="container">
         <Container className="pt-3">
           <Row>
             <Col md={3}>
-              <Image src="https://www.gravatar.com/avatar/00000000000000000000000000000000" roundedCircle width="100%"/>
+              <Image src={userInfo.current["avatarURL"]} roundedCircle width="100%"/>
             </Col>
             <Col md={9}>
               <div className={"pt-4"}>
                 <h3>{userInfo.current["username"]}</h3>
                 <h3>{userInfo.current["email"]}</h3>
-
+                <h3>{userInfo.current["createdAt"]}</h3>
               </div>
             </Col>
           </Row>
