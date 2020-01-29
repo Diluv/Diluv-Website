1d700cf924a32ace83d491d460b033b3f7c73458
Jared <jaredlll08@gmail.com>
2020-01-29 15:05:34 +0200

    dropdown stays open when it should

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,16 +1,21 @@
-import React, {ReactNode, useState} from "react";
+import React, {ReactNode, useEffect, useRef, useState} from "react";
 
 function DropDown(props: { name: string, children: ReactNode, className?: string }) {
 
-  const [expanded, setExpanded] = useState(false);
-  return (<React.Fragment>
-    <div className={(props.className ? props.className + " " : "") + "pb-1"}>
-      <p className={"cursor-pointer "} onClick={() => setExpanded(!expanded)}>{props.name}</p>
+  const {
+    ref,
+    isComponentVisible,
+    setIsComponentVisible
+  }: { ref: React.Ref<any>, isComponentVisible: boolean, setIsComponentVisible: Function } = useComponentVisible(false);
+
+  return (<div ref={ref}>
+    <div className={(props.className ? props.className + " " : "") + "pb-1"} >
+      <p className={"cursor-pointer "}  onClick={() => setIsComponentVisible(!isComponentVisible)}>{props.name}</p>
     </div>
-    <div className={"fixed block bg-white mr-4  md:right-0 left-auto sm:inset-x-0 md:inset-auto" + (expanded ? "" : " hidden")}>
+    <div className={"fixed block bg-white mr-4  md:right-0 left-auto sm:inset-x-0 md:inset-auto" + (isComponentVisible ? "" : " hidden")} >
       {props.children}
     </div>
-  </React.Fragment>);
+  </div>);
 }
 
 export function DropDownItem(props: { children: ReactNode, className?: string }) {
@@ -21,6 +26,37 @@ export function DropDownItem(props: { children: ReactNode, className?: string })
 
 export function DropDownSpacer() {
   return <div className={"border-black-300 border-b"}/>
+}
+
+function useComponentVisible(initialIsVisible: false) {
+  const [isComponentVisible, setIsComponentVisible] = useState(
+    initialIsVisible
+  );
+  const ref = useRef(null);
+
+  const handleHideDropdown = (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      setIsComponentVisible(false);
+    }
+  };
+
+  const handleClickOutside = (event: { target: any; }) => {
+    // @ts-ignore
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleHideDropdown, true);
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("keydown", handleHideDropdown, true);
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
+
+  return {ref, isComponentVisible, setIsComponentVisible};
 }
 
 export default DropDown;
