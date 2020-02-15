0fb17183c5c4d324932129c417af6214beb909d1
lclc98 <lclc98@lclc98.com>
2020-02-15 20:59:33 +1000

    Added Nodecraft logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React, {useContext} from "react";
 import Drop from "../components/Drop"
+import Nodecraft from "../components/Nodecraft"
 import {Theme} from "../utils/Contexts";
 
 function Footer() {
@@ -13,11 +14,11 @@ function Footer() {
   }
   return (<footer className={"pt-4 py-md-5 md:pt-5 pb-8 font-hero " + themeBgClass}>
     <div className={"flex flex-wrap justify-center text-center md:text-left"}>
-      <div className="w-1/2 pt-2 md:w-1/6 md:ml-auto text-center md:text-left">
+      <div className="w-1/2 pt-2 md:w-1/12 md:ml-auto text-center md:text-left">
         <Drop className={"mx-auto md:mx-0"} height={"54px"} width={"27px"}/>
         <small className="mb-3 text-gray-600 mx-auto md:mx-0">© {year}</small>
       </div>
-      <div className="w-1/2 pt-2 md:w-1/6">
+      <div className="w-1/2 pt-2 md:w-1/12">
         <h5 className={"text-xl font-bold pb-2"}>Product</h5>
         <ul className="list-none items-center">
           <li><a href="/news" className={" hover:text-white transition-colors duration-150 ease-in"}>News</a></li>
@@ -39,7 +40,7 @@ function Footer() {
           <li><a href="https://github.com/Diluv" className={"hover:text-white transition-colors duration-150 ease-in"}>Github</a></li>
         </ul>
       </div>
-      <div className="w-1/2 pt-2 md:w-1/6">
+      <div className="w-1/2 pt-2 md:w-1/12">
         <h5 className={"text-xl font-bold pb-2"}>About</h5>
         <ul className="list-none">
           <li><a href="/about" className={"hover:text-white transition-colors duration-150 ease-in"}>About</a></li>
@@ -47,7 +48,7 @@ function Footer() {
           <li><a href="/terms" className={" hover:text-white transition-colors duration-150 ease-in"}>Terms</a></li>
         </ul>
       </div>
-      <div className="w-1/2 pt-2 md:w-1/6 mr-auto">
+      <div className="w-1/2 pt-2 md:w-1/12">
         <h5 className={"text-xl font-bold pb-2 "}>Contact</h5>
         <ul className="list-none">
           <li><a href="https://twitter.com/DiluvSupport" className={"hover:text-white transition-colors duration-150 ease-in"}>Twitter</a></li>
@@ -58,6 +59,12 @@ function Footer() {
             )
           }
         </ul>
+      </div>
+      <div className="w-1/2 pt-2 md:w-1/12 mr-auto">
+        <h5 className={"text-xl font-bold pb-2"}>Sponsored</h5>
+        <a href={"https://nodecraft.com"}>
+          <Nodecraft/>
+        </a>
       </div>
     </div>
   </footer>);
