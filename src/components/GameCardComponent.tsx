19857838d71dc0842d9c9ae3a73ba7a1ec059807
Jared <jaredlll08@gmail.com>
2020-02-23 04:33:10 +0200

    made GameCardComponent use tailwind

--- src/components/GameCardComponent.tsx
+++ src/components/GameCardComponent.tsx
@@ -9,11 +9,10 @@ function GameCardComponent({
   screenshot = 'https://images.placeholders.dev/?width=348&height=225',
 }: Props) {
   return (
-    <div className="card mb-4 box-shadow">
+    <div className="mb-4">
       <img
-        className="card-img-top"
+        className="w-full"
         alt="game/mod name"
-        style={{ height: 225, width: '100%', display: 'block' }}
         src={screenshot}
       />
     </div>
