d6a5f04226586baf4ca91262e3b466f08bbe8498
Jared <jaredlll08@gmail.com>
2020-02-27 15:27:46 +0200

    Mod cards are starting to look usable

--- src/components/ModCard.tsx
+++ src/components/ModCard.tsx
@@ -3,30 +3,31 @@ import { Theme } from "../utils/context";
 
 type Props = {
   name: string
+  author: string
   screenshot: string
+  summary: string
 };
 
 function ModCard({
   name = 'This is the default title',
+  author = "Creator",
   screenshot = 'https://images.placeholders.dev/?width=348&height=225',
+  summary = "This is the summary"
 }: Props) {
   const theme = useContext(Theme);
   return (
-    <div className={"flex mb-4 box-shadow rounded border " +(theme.theme === "dark" ? "bg-dark-600 border-lighten-100" : "bg-gray-200 border-darken-100")}>
-      <img
-        className="w-1/2"
-        alt="game/mod name"
-        style={{ width: '100%' }}
-        src={screenshot}
-      />
-      <div className="w-1/2">
-        <p className="card-text">{name}</p>
-        <div className="d-flex justify-content-between align-items-center">
-          <div className="btn-group">
-            <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+    <div className={"flex border " + (theme.theme === "dark" ? "bg-gray-700 border-gray-600 text-gray-100 shadow-light" : "bg-gray-300 border-gray-500 text-black shadow")}>
+      <div className={"flex flex-col w-full"}>
+        <p className={"font-light text-xs text-center py-2 border-b border-darken-200 bg-darken-200"}><span className="font-bold text-lg">{name}</span> by <span className={"text-sm"}>{author}</span></p>
+        <div className={"flex flex-row"}>
+          <img
+            className="w-1/3 mr-2"
+            alt="game/mod name"
+            src={screenshot}
+          />
+          <div className="w-2/2 mt-1">
+            <p className={"text-sm"}>{summary}</p>
           </div>
-          <small className="text-muted">9 mins</small>
         </div>
       </div>
     </div>
