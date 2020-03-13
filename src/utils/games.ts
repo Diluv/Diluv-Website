c13364b4c5d1d9658314e93503252979a9c24276
Jared <jaredlll08@gmail.com>
2020-03-13 17:58:40 +0200

    Added methods to get featured games / projects

--- src/utils/games.ts
+++ src/utils/games.ts
@@ -12,6 +12,16 @@ export async function getGames(): Promise<Game[]> {
   }
 }
 
+export async function getFeaturedGames(): Promise<Game[]> {
+  try {
+    const data: Data<Game[]> = await get(`${API_URL}/v1/featured/games`)
+      .then((Response) => Promise.resolve(Response.data).catch((reason) => Promise.resolve(reason)));
+    return data.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
 export async function getGamesBySlug(slug: string | string[]): Promise<Game> {
   try {
     const data: Data<Game> = await get(`${API_URL}/v1/games/${slug}`)
--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -15,11 +15,21 @@ export async function getProjectType(gameSlug: string, projectTypeSlug: string):
   }
 }
 
-
 export async function getProjects(gameSlug: string, projectTypeSlug: string, page = 1, limit = 20): Promise<Project[]> {
   try {
     let cursor = encodeURI(new Buffer(JSON.stringify({offset: (page-1)*limit})).toString("base64"));
     const data: Data<Project[]> = await get(`${API_URL}/v1/games/${gameSlug}/${projectTypeSlug}/projects?cursor=${cursor}&limit=${limit}`)
+      .then((Response) => Promise.resolve(Response.data).catch((reason) => Promise.resolve(reason)));
+    return data.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+export async function getFeaturedProjects(): Promise<Project[]> {
+  try {
+    console.log(`${API_URL}/v1/featured/projects`);
+    const data: Data<Project[]> = await get(`${API_URL}/v1/featured/projects`)
       .then((Response) => Promise.resolve(Response.data).catch((reason) => Promise.resolve(reason)));
     return data.data;
   } catch (err) {
