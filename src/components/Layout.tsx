63e79734150b313c97a4065aa67be0621af43e66
Jared <jaredlll08@gmail.com>
2020-05-10 00:19:21 +0200

    use simplebar instead of browser scroll bars

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import Head from "next/head";
+import SimpleBar from 'simplebar-react';
 
 type Props = {
   children: JSX.Element | JSX.Element[]
@@ -14,19 +15,21 @@ function Layout({
 }: Props) {
 
   return (
-    <div className={`min-h-screen flex flex-col`}>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8"/>
-      </Head>
-      <header>
-        <NavBar/>
-      </header>
-      <main className={`flex-grow dark:bg-gray-800 dark:text-white`}>
-        {children}
-      </main>
-      <Footer/>
-    </div>
+    <SimpleBar style={{ maxHeight: `100vh` }}>
+      <div className={`min-h-screen flex flex-col`}>
+        <Head>
+          <title>{title}</title>
+          <meta charSet="utf-8"/>
+        </Head>
+        <header>
+          <NavBar/>
+        </header>
+        <main className={`flex-grow dark:bg-gray-800 dark:text-white`}>
+          {children}
+        </main>
+        <Footer/>
+      </div>
+    </SimpleBar>
   );
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../css/index.css"
 import React from "react";
 import { AppProps } from "next/app";
+import 'simplebar/dist/simplebar.min.css';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
