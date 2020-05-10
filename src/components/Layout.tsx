a2778ca229c0bd3e6c2de8c6838eb8d01ff225ec
Jared <jaredlll08@gmail.com>
2020-05-10 06:52:17 +0200

    fixed simplebar hopefully

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,7 @@ function Layout({
 }: Props) {
 
   return (
-    <SimpleBar style={{ maxHeight: `100vh` }}>
+    <SimpleBar style={{ minHeight: "100vh", maxHeight: `100vh` }}>
       <div className={`min-h-screen flex flex-col`}>
         <Head>
           <title>{title}</title>
