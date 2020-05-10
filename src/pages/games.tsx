1085bbfd17ff0e8e8565d3ab12031169fa7c4231
Jared <jaredlll08@gmail.com>
2020-05-10 06:37:03 +0200

    fix games being weird with sort and search

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -30,8 +30,8 @@ export default function IndexPage() {
                 <div className={`flex flex-row`}>
                   <label className={"p-1 pr-2 my-auto font-bold"} htmlFor={"searchBox"}>Search games: </label>
                   <div className={"relative my-auto"}>
-                    <Search className={`ml-2 my-2 fill-current absolute svg-icon pointer-events-none transition-opacity duration-300 ${search.trim().length ? `text-diluv-500` : ``} ${selectedField === "searchBox" ? "opacity-0 ease-out" : "opacity-100 ease-in"}`} width={"1rem"} height={"1rem"}/>
-                    <input className={"p-1 bg-transparent "} type={"text"} placeholder={"Search games"} id={"searchBox"} style={{ textIndent: "2rem" }} onFocus={onFocus} onBlur={onBlur} onChange={event => setSearch(event.target.value)}/>
+                    <Search className={`ml-2 my-2 fill-current absolute svg-icon pointer-events-none transition-opacity duration-300 ${search.trim().length ? `text-diluv-500` : `text-black`} ${selectedField === "searchBox" ? "opacity-0 ease-out" : "opacity-100 ease-in"}`} width={"1rem"} height={"1rem"}/>
+                    <input className={"p-1 bg-transparent"} type={"text"} placeholder={"Search games"} id={"searchBox"} style={{ textIndent: "2rem" }} onFocus={onFocus} onBlur={onBlur} onChange={event => setSearch(event.target.value)}/>
                   </div>
                 </div>
                 <div>
@@ -39,8 +39,8 @@ export default function IndexPage() {
                     <label className={"p-1 pr-2 my-auto font-bold"} htmlFor={"sortBox"}>Sort: </label>
                     <div className={"relative my-auto"}>
                       <Filter className={`pointer-events-none ml-2 my-2 fill-current absolute svg-icon pointer-events-none transition-colours duration-300 ease-in-out ${selectedField === "sortBox" ? "text-diluv-500" : ""}`} width={"1rem"} height={"1rem"}/>
-                      <select className={`p-1 mr-16 appearance-none bg-transparent w-full`} id={`sortBox`} style={{ textIndent: "2rem" }} onFocus={onFocus} onBlur={onBlur} onChange={event => setSearch(event.target.value)}>
-                        <option className={`text-black `}>
+                      <select className={`p-1 mr-16 appearance-none bg-transparent w-full`} id={`sortBox`} style={{ textIndent: "2rem" }} onFocus={onFocus} onBlur={onBlur} onChange={event => setSort(event.target.value)}>
+                        <option className={`text-black`}>
                           Popularity
                         </option>
                         <option className={`text-black`}>
