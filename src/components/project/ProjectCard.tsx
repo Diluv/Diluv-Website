58be18dcfd533d47f9cbf63585271a3ae41ce156
Jared <jaredlll08@gmail.com>
2020-06-08 04:28:18 +0200

    started mobile view for project list and markdown tests for projects

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -27,83 +27,94 @@ function ProjectCard({ gameSlug, projectTypeSlug, project, tagFilter, setTagFilt
     let projectUrlRef = `/games/[GameSlug]/[ProjectType]/[ProjectSlug]`;
     let projectUrl = `/games/${gameSlug}/${projectTypeSlug}/${project.slug}`;
     return <>
-        <div className={`grid my-4 grid-cols-3`} style={{ gridTemplateColumns: "8rem 1fr" }}>
-            <Link href={projectUrlRef} as={projectUrl}>
-                <a>
-                    <img src={project.logo} className={` h-32`}/>
-                </a>
-            </Link>
-            <div className={`ml-4 grid`} style={{ gridTemplateRows: "0.5fr 0.75fr 0.5fr" }}>
-                <div>
-                    <Link href={projectUrlRef} as={projectUrl}>
-                        <a>
-                            <h4 className={`font-semibold`}>{project.name}</h4>
-                        </a>
-                    </Link>
-                    <div className={`text-gray-600`}>
+        <div className={`grid my-4`} style={{
+            gridTemplateAreas:
+                `"image image image image"
+                 "header header header header"
+                 "summary summary summary summary"
+                 "downloads downloads downloads downloads"
+                 "created created created created"
+                 "updated updated updated updated"
+                 "tag1 tag2 tag3 tag4"
+                 `,
+            gridTemplateColumns: `auto auto auto auto`,
+            gridTemplateRows: `16rem auto auto auto auto auto`
+        }}>
+            <div style={{ gridArea: "image" }}>
+                <Link href={projectUrlRef} as={projectUrl}>
+                    <a>
+                        <img src={project.logo} className={`mx-auto h-64 w-64`}/>
+                    </a>
+                </Link>
+            </div>
+
+            <div className={"ml-2 leading-snug"} style={{ gridArea: "header" }}>
+                <Link href={projectUrlRef} as={projectUrl}>
+                    <a>
+                        <h4 className={`font-semibold`}>{project.name}</h4>
+                    </a>
+                </Link>
+                <div className={`text-gray-600`}>
                         <span>
                             {`by `}
                         </span>
-                        {listContributors(project)}
-                    </div>
-                </div>
-                <div>
-                    <p> {project.summary}</p>
-                </div>
-                <div className={`grid gap-4`} style={{ gridTemplateColumns: "auto auto auto auto 1fr" }}>
-                    <div className={`my-auto text-center`}>
-                        <Tippy content={<div className={`bg-gray-800 border border-gray-900 text-white opacity-90 p-1 text-center`}>
-                            <p>
-                                {project.downloads} Downloads
-                            </p>
-                        </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
-                            <div className={`flex mx-auto`}>
-                                <ChartBar className={`fill-current mr-1 my-auto`} width={`1rem`} height={`1rem`}/>
-                                <span className={`cursor-default`}>
-                                {project.downloads}
-                            </span>
-                            </div>
-                        </Tippy>
-                    </div>
-                    <div className={`my-auto text-center`}>
-                        <Tippy content={<div className={`bg-gray-800 border border-gray-900 text-white opacity-90 p-1 text-center`}>
-                            <p>
-                                Created On
-                            </p>
-                            <p>{moment(project.createdAt).format("MMMM Do YYYY")}</p>
-                        </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
-                            <div className={`flex mx-auto`}>
-                                <HourGlass className={`fill-current mr-1 my-auto`} width={`1rem`} height={`1rem`}/>
-                                <span className={`cursor-default`}>
-                                {moment(project.createdAt).fromNow()}
-                            </span>
-                            </div>
-                        </Tippy>
-                    </div>
-                    <div className={`my-auto text-center`}>
-                        <Tippy content={<div className={`bg-gray-800 border border-gray-900 text-white opacity-90 p-1 text-center`}>
-                            <p>
-                                Updated On
-                            </p>
-                            <p>{moment(project.updatedAt).format("MMMM Do YYYY")}</p>
-                        </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
-                            <div className={`flex mx-auto`}>
-                                <Time className={`fill-current mr-1 my-auto`} width={`1rem`} height={`1rem`}/>
-                                <span className={`cursor-default`}>
-                                {moment(project.updatedAt).fromNow()}
-                            </span>
-                            </div>
-                        </Tippy>
-                    </div>
-
-                    <div className={`my-auto text-center`}>
-                        <div className={`grid cursor-default  gap-2`} style={{ gridTemplateColumns: "auto auto auto auto" }}>
-                            {project.tags.map(value => <FilterTag key={value.slug} tagSlug={value.slug} tagName={value.name} tagFilter={tagFilter}
-                                                                  setTagFilter={setTagFilter}/>)}
-                        </div>
-                    </div>
+                    {listContributors(project)}
                 </div>
             </div>
+
+            <div className={"ml-2"} style={{ gridArea: "summary" }}>
+                <p> {project.summary}</p>
+            </div>
+
+            <div className={`ml-2 my-auto text-center mr-2`} style={{ gridArea: "downloads" }}>
+                <Tippy content={<div className={`bg-gray-800 border border-gray-900 text-white opacity-90 p-1 text-center`}>
+                    <p>
+                        {project.downloads} Downloads
+                    </p>
+                </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
+                    <div className={`flex mx-auto`}>
+                        <ChartBar className={`fill-current mr-1 my-auto`} width={`1rem`} height={`1rem`}/>
+                        <span className={`cursor-default`}>
+                                {project.downloads}
+                            </span>
+                    </div>
+                </Tippy>
+            </div>
+
+            <div className={`my-auto text-center mr-2`} style={{ gridArea: "created" }}>
+                <Tippy content={<div className={`bg-gray-800 border border-gray-900 text-white opacity-90 p-1 text-center`}>
+                    <p>
+                        Created On
+                    </p>
+                    <p>{moment(project.createdAt).format("MMMM Do YYYY")}</p>
+                </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
+                    <div className={`flex mx-auto`}>
+                        <HourGlass className={`fill-current mr-1 my-auto`} width={`1rem`} height={`1rem`}/>
+                        <span className={`cursor-default`}>
+                                {moment(project.createdAt).fromNow()}
+                            </span>
+                    </div>
+                </Tippy>
+            </div>
+            <div className={`my-auto text-center mr-2`} style={{ gridArea: "updated" }}>
+                <Tippy content={<div className={`bg-gray-800 border border-gray-900 text-white opacity-90 p-1 text-center`}>
+                    <p>
+                        Updated On
+                    </p>
+                    <p>{moment(project.updatedAt).format("MMMM Do YYYY")}</p>
+                </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
+                    <div className={`flex mx-auto`}>
+                        <Time className={`fill-current mr-1 my-auto`} width={`1rem`} height={`1rem`}/>
+                        <span className={`cursor-default`}>
+                                {moment(project.updatedAt).fromNow()}
+                            </span>
+                    </div>
+                </Tippy>
+            </div>
+
+                {project.tags.map((value, i) => <div className={`my-auto cursor-default text-center`} key={value.slug} style={{ gridArea: `tag${i+1}` }}>
+                    <FilterTag tagSlug={value.slug} tagName={value.name} tagFilter={tagFilter}
+                               setTagFilter={setTagFilter}/></div>)}
 
 
         </div>
