d04ed8475a86e076bd40051f6350113e59fbfb56
Jared <jaredlll08@gmail.com>
2020-06-11 20:21:20 +0200

    fix tag colours

--- src/components/misc/FilterTag.tsx
+++ src/components/misc/FilterTag.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import { SelectData } from "../../interfaces";
 
 export function FilterTag({ tagName, tagSlug, tagFilter, setTagFilter }: { tagName: string, tagSlug: string, tagFilter: SelectData[], setTagFilter: Function }) {
-    function onClick() {
-        let valid = true;
-        let newData = tagFilter;
+    function hasTag(){
+        let hasTag = false;
         for (let data of tagFilter) {
             if (data.value === tagSlug) {
-                valid = false;
+                hasTag = true;
             }
         }
-        if (valid)
+        return hasTag;
+    }
+    function onClick() {
+        let newData = tagFilter;
+        if (!hasTag())
             newData = newData.concat({ value: tagSlug, label: tagName });
         setTagFilter(newData);
     }
 
-    return <div className={`inline flex cursor-pointer px-2 align-middle bg-hsl-900 hover:bg-tag text-hsl-200`} onClick={onClick}>
+    return <div className={`inline flex cursor-pointer px-2 align-middle ${!hasTag() ? `bg-hsl-100` : `bg-tag`} hover:bg-tag text-hsl-800`} onClick={onClick}>
         <span>{tagName}</span>
 
     </div>;
@@ -23,7 +26,7 @@ export function FilterTag({ tagName, tagSlug, tagFilter, setTagFilter }: { tagNa
 
 
 export function DisplayTag({ tagName, tagSlug }: { tagName: string, tagSlug: string }) {
-    return <div className={`inline flex cursor-default px-2 align-middle bg-hsl-900 hover:bg-tag text-hsl-200`}>
+    return <div className={`inline flex cursor-default px-2 align-middle bg-hsl-100 hover:bg-tag text-hsl-800`}>
         <span>{tagName}</span>
 
     </div>;
