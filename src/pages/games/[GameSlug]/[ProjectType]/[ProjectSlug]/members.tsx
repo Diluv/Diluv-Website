987107a9ba6430102fb0914671b61d43b5d6e87e
Jared <jaredlll08@gmail.com>
2020-06-14 01:46:03 +0200

    member page done

--- src/pages/games/[GameSlug]/[ProjectType]/[ProjectSlug]/members.tsx
+++ src/pages/games/[GameSlug]/[ProjectType]/[ProjectSlug]/members.tsx
@@ -4,7 +4,6 @@ import { NextPageContext } from "next";
 import { get } from "../../../../../utils/request";
 import { API_URL } from "../../../../../utils/api";
 import { Project } from "../../../../../interfaces";
-import ReactMarkdown from "react-markdown";
 import ProjectInfo from "../../../../../components/project/ProjectInfo";
 
 export default function Files({ project }: { project: Project }) {
@@ -15,8 +14,20 @@ export default function Files({ project }: { project: Project }) {
                 <div className={`mx-auto w-5/6 md:w-4/6`}>
                     <ProjectInfo project={project} pageType={"members"}/>
                     <div id={"pageContent"}>
-                        <div className={`p-4`}>
-                           members here
+                        <div className={`py-4`}>
+                            <div className={`w-1/2`}>
+                                {project.contributors.map(value => {
+                                    return <div key={value.userId} className={`grid col-gap-2 my-1`} style={{gridTemplateAreas: `"avatar name" "avatar role"`, gridTemplateRows: "1.5rem 2.5rem", gridTemplateColumns: "4rem auto"}}>
+                                        <img className={`w-16 h-16`} src={value.avatarURL} style={{gridArea: "avatar"}}/>
+                                        <p style={{gridArea: "name"}}>
+                                            {value.displayName}
+                                        </p>
+                                        <p style={{gridArea: "role"}}>
+                                            Role: {value.role}
+                                        </p>
+                                    </div>;
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -24,7 +35,6 @@ export default function Files({ project }: { project: Project }) {
         </Layout>
     );
 }
-
 
 export async function getServerSideProps(context: NextPageContext) {
     let { GameSlug, ProjectType, ProjectSlug } = context.query;
