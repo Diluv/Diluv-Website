f5aa534b59cb71ed00acda801d3c54c3bf330a4c
Tyler Hancock <darklime@live.ca>
2020-06-20 18:12:00 -0600

    Switched markdown related plugins to use the new toolchain.

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,45 @@
+import remark from 'remark'
+// @ts-ignore
+import reactRenderer from 'remark-react'
+import merge from 'deepmerge';
+import github from 'hast-util-sanitize/lib/github.json';
+// @ts-ignore
+import html from 'remark-html'
+// @ts-ignore
+import slug from 'remark-slug'
+// @ts-ignore
+import headings from 'remark-autolink-headings'
+// @ts-ignore
+import underline from 'remark-underline'
+// @ts-ignore
+import spoiler from 'remark-spoiler'
+// @ts-ignore
+import admonitions from 'remark-admonitions'
+
+type Props = {
+    markdown: string
+};
+
+const schema = merge(github, {
+    clobberPrefix: '',
+    attributes: { '*': ['className'] }, // Allows className through the filter
+});
+
+function Markdown({ markdown }: Props) {
+
+    return remark()
+        .use(slug) // GitHub like anchor slugs for headings.
+        .use(headings) // Applies references for slug anchors. May be an issue here with how it's being applied as it throws a console error.
+        .use(admonitions) // Adds support for notices/admonitions
+        .use(underline) // Adds underlined text support
+        .use(spoiler) // Adds spoiler text support
+        .use(html) // Renders previous stuff into HTML where approperiat
+        .use(reactRenderer, { // Renders to react fragments.
+            sanitize: schema
+        })
+        .processSync(markdown)
+        // @ts-ignore
+        .result;
+}
+
+export default Markdown;
