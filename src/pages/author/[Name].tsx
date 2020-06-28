b44d26269efe8c97f54a8b92dde08ba69cb3da29
Jared <jaredlll08@gmail.com>
2020-06-28 03:55:50 +0200

    basic author page

--- src/pages/author/[Name].tsx
+++ src/pages/author/[Name].tsx
@@ -5,13 +5,35 @@ import { Author, HasTheme } from "../../interfaces";
 import { getTheme } from "../../utils/theme";
 import { get } from "../../utils/request";
 import { API_URL } from "../../utils/api";
+import moment from "moment";
+import { followCursor } from "tippy.js";
+import Tippy from "@tippyjs/react";
 
 export default function ProjectIndex({ theme, author }: { author: Author } & HasTheme) {
-    console.log(author);
-    return (
-        <Layout title={author.displayName} theme={theme}>
-            <div>
-                <img src={author.avatarURL}/>
+    return (<Layout title={author.displayName} theme={theme}>
+            <div className={`container mx-auto my-8`}>
+                <div className={`w-11/12 mx-auto`}>
+                    <div className={`grid col-gap-2 row-gap-2 sm:row-gap-0 profilePageSmall sm:profilePageLarge`}>
+                        <div className={`area-image`}>
+                            <img src={author.avatarURL}/>
+                        </div>
+                        <div className={`area-summary`}>
+                            <h3>{author.displayName}</h3>
+
+                            <Tippy content={
+                                <div
+                                    className={`bg-gray-800 border border-gray-900 dark:border-dark-100 text-white opacity-90 p-1 text-center`}>
+                                    {moment(author.createdAt).calendar()}
+                                </div>} followCursor={true} plugins={[followCursor]} duration={0} hideOnClick={false}>
+                                <div>
+                                    Joined {moment(author.createdAt).fromNow()}
+                                </div>
+                            </Tippy>
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </Layout>
     );
