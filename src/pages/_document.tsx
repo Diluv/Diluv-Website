d3719d42cbb1aaf765cb5439d10b2467970003a3
Jared <jaredlll08@gmail.com>
2020-07-05 23:48:56 +0200

    Use the new logo for favicon purposes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,21 @@ class MyDocument extends Document {
 
         return (
             <Html>
-                <Head lang={"en"}/>
+                <Head lang={"en"}>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1"/>
+                    <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png?v=1"/>
+                    <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png?v=1"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1"/>
+                    <link rel="manifest" href="/site.webmanifest?v=1"/>
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#3da6e4"/>
+                    <link rel="shortcut icon" href="/favicon.ico?v=1"/>
+                    <meta name="apple-mobile-web-app-title" content="Diluv"/>
+                    <meta name="application-name" content="Diluv"/>
+                    <meta name="msapplication-TileColor" content="#2b5797"/>
+                    <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=1"/>
+                    <meta name="theme-color" content="#1b4b67"/>
+                </Head>
                 <body className={`min-h-100vh`}>
                 <Main/>
                 <NextScript/>
