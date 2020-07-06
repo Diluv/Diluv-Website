0e640d941965202818ac4f91694758a33b53d34a
lclc98 <lclc98@lclc98.com>
2020-07-06 22:42:15 +1000

    Set domain for theme cookie

--- src/pages/api/set_theme.ts
+++ src/pages/api/set_theme.ts
@@ -3,11 +3,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 module.exports = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!req.body["theme"]) {
-        res.writeHead(400).end(`Bad request! theme: ${req.body["theme"]}`);
+        res.writeHead(400);
+        res.end(`Bad request! theme: ${req.body["theme"]}`);
         return;
     }
     let expiresAt: Date = new Date(9999, 12, 31, 23, 59, 59);
-    let options: CookieSerializeOptions = { path: '/', expires: expiresAt };
+    const domain = process.env.NODE_ENV == 'production' ? 'diluv.com' : undefined;
+    let options: CookieSerializeOptions = { path: '/', expires: expiresAt, secure: true, sameSite: 'lax', domain: domain };
     res.setHeader('Set-Cookie', [serialize('theme', req.body["theme"], options)]);
     res.end();
 };
