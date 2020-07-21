7510a00566d5fc30e20f26c48bfc6dc832470b75
Jared <jaredlll08@gmail.com>
2020-07-21 09:48:16 +0200

    css cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-    purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts',], theme: {
+    purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts',],
+    theme: {
         extend: {
             textIndent: (theme, {negative}) => ({
                 ...{
@@ -19,18 +20,6 @@ module.exports = {
                 "0.125": "2px",
             },
             gridTemplateColumns: {
-                "1-auto": "repeat(1, auto)",
-                "2-auto": "repeat(2, auto)",
-                "3-auto": "repeat(3, auto)",
-                "4-auto": "repeat(4, auto)",
-                "5-auto": "repeat(5, auto)",
-                "6-auto": "repeat(6, auto)",
-                "7-auto": "repeat(7, auto)",
-                "8-auto": "repeat(8, auto)",
-                "9-auto": "repeat(9, auto)",
-                "10-auto": "repeat(10, auto)",
-                "11-auto": "repeat(11, auto)",
-                "12-auto": "repeat(12, auto)",
                 "project-4": "0.25fr 1.25fr 0.5fr 1fr",
                 "project-types": "1fr auto auto auto 1fr",
                 "auto": "repeat(auto-fill, minmax(8.3%,1fr));",
@@ -41,18 +30,6 @@ module.exports = {
                 'project-type-nav': "2.5fr 0.5fr"
             },
             gridTemplateRows: {
-                "1-auto": "repeat(1, auto)",
-                "2-auto": "repeat(2, auto)",
-                "3-auto": "repeat(3, auto)",
-                "4-auto": "repeat(4, auto)",
-                "5-auto": "repeat(5, auto)",
-                "6-auto": "repeat(6, auto)",
-                "7-auto": "repeat(7, auto)",
-                "8-auto": "repeat(8, auto)",
-                "9-auto": "repeat(9, auto)",
-                "10-auto": "repeat(10, auto)",
-                "11-auto": "repeat(11, auto)",
-                "12-auto": "repeat(12, auto)",
                 "project-4": "0.25fr 1fr 0.60fr 1fr",
                 "project-types": "1fr auto auto auto 1fr",
                 "auto": "repeat(auto-fill, minmax(8.3%,1fr));",
@@ -76,14 +53,7 @@ module.exports = {
                 "20": "2rem"
             },
             minHeight: {
-                "0vh": "0vh",
-                "25vh": "25vh",
-                "50vh": "50vh",
-                "70vh": "70vh",
                 "100vh": "100vh",
-                "1/4": "25%",
-                "1/2": "50%",
-                "3/4": "70%",
                 "10": "2.5rem",
                 "12": "3rem",
                 "24": "6rem",
@@ -157,77 +127,12 @@ module.exports = {
                     800: '#2e2e2e',
                     850: '#242424',
                     900: '#1b1b1b',
-                },
-                "danger": {
-                    100: "#FDEEEF",
-                    200: "#FAD6D8",
-                    300: "#F7BDC1",
-                    400: "#F28B92",
-                    500: "#EC5963",
-                    600: "#D45059",
-                    700: "#8E353B",
-                    800: "#6A282D",
-                    900: "#471B1E"
-                },
-                "warning": {
-                    100: "#FDFAEE",
-                    200: "#FAF4D5",
-                    300: "#F6EDBB",
-                    400: "#F0DF88",
-                    500: "#E9D155",
-                    600: "#D2BC4D",
-                    700: "#8C7D33",
-                    800: "#695E26",
-                    900: "#463F1A"
-                },
-                "success": {
-                    100: "#EDFCF0",
-                    200: "#D3F7DB",
-                    300: "#B8F2C5",
-                    400: "#82E999",
-                    500: "#4DDF6D",
-                    600: "#45C962",
-                    700: "#2E8641",
-                    800: "#236431",
-                    900: "#174321"
-                },
-                "info": {
-                    100: "#EBFBFF",
-                    200: "#CDF4FE",
-                    300: "#AFEDFD",
-                    400: "#73E0FC",
-                    500: "#37D2FA",
-                    600: "#32BDE1",
-                    700: "#217E96",
-                    800: "#195F71",
-                    900: "#113F4B"
-                },
-                "darken": {
-                    100: "rgba(0,0,0,0.1)",
-                    200: "rgba(0,0,0,0.2)",
-                    300: "rgba(0,0,0,0.3)",
-                    400: "rgba(0,0,0,0.4)",
-                    500: "rgba(0,0,0,0.5)",
-                    600: "rgba(0,0,0,0.6)",
-                    700: "rgba(0,0,0,0.7)",
-                    800: "rgba(0,0,0,0.8)",
-                    900: "rgba(0,0,0,0.9)"
-                },
-                "lighten": {
-                    100: "rgba(255,255,255,0.1)",
-                    200: "rgba(255,255,255,0.2)",
-                    300: "rgba(255,255,255,0.3)",
-                    400: "rgba(255,255,255,0.4)",
-                    500: "rgba(255,255,255,0.5)",
-                    600: "rgba(255,255,255,0.6)",
-                    700: "rgba(255,255,255,0.7)",
-                    800: "rgba(255,255,255,0.8)",
-                    900: "rgba(255,255,255,0.9)"
                 }
             }
 
         }
-    }, variants: {
+    },
+    variants: {
         fill: ["responsive", "hover"],
         opacity: ["responsive", "hover", "focus", "disabled"],
         cursor: ["responsive", "disabled"],
@@ -237,14 +142,18 @@ module.exports = {
         borderWidth: ["responsive", "hover", "focus"],
         borderColor: ["responsive", "hover", "focus", "active", "group-hover", "dark", "dark-hover", "dark-active", "dark-focus"],
         textIndent: ['responsive']
-    }, plugins: [require("tailwindcss-dark-mode")(), require('tailwindcss-text-indent')(), plugin(function ({addBase, config}) {
-        addBase({
-            "h1": {fontSize: config("theme.fontSize.4xl")},
-            "h2": {fontSize: config("theme.fontSize.2xl")},
-            "h3": {fontSize: config("theme.fontSize.lg")},
-            "h4": {fontSize: config("theme.fontSize.base")},
-            "h5": {fontSize: config("theme.fontSize.sm")},
-            "h6": {fontSize: config("theme.fontSize.xs")}
-        });
-    })]
+    },
+    plugins: [
+        require("tailwindcss-dark-mode")(),
+        require('tailwindcss-text-indent')(),
+        plugin(function ({addBase, config}) {
+            addBase({
+                "h1": {fontSize: config("theme.fontSize.4xl")},
+                "h2": {fontSize: config("theme.fontSize.2xl")},
+                "h3": {fontSize: config("theme.fontSize.lg")},
+                "h4": {fontSize: config("theme.fontSize.base")},
+                "h5": {fontSize: config("theme.fontSize.sm")},
+                "h6": {fontSize: config("theme.fontSize.xs")}
+            });
+        })]
 };
