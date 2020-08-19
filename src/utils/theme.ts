87ea649050c9265bd817137de078473ba3ceabea
Jared <jaredlll08@gmail.com>
2020-08-19 18:57:04 +0200

    fix react-select style

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -4,6 +4,9 @@ import { Theme } from "../interfaces";
 import { parse } from "cookie";
 import { State } from "react-select/base";
 import { OptionTypeBase } from "react-select/src/types";
+import { Styles, StylesConfig } from "react-select";
+import { ControlProps } from "react-select/src/components/Control";
+import { StylesConfigFunction } from "react-select/src/styles";
 
 export function getTheme(context: GetServerSidePropsContext): Theme {
     let theme = "light";
@@ -20,17 +23,19 @@ export function getTheme(context: GetServerSidePropsContext): Theme {
     return { theme };
 }
 
-export const reactSelectStyle = {
-    control: (provided: CSSProperties, state: State<OptionTypeBase>): Partial<CSSProperties> => ({
-        ...provided,
-        "borderRadius": 0,
-        "borderColor": state.isFocused ? "#A0AEC0" : "#CBD5E0",
-        "boxShadow": "none",
-        // @ts-ignore
-        "&:hover": {
-            borderColor: "#A0AEC0"
-        }
-    }),
+export const reactSelectStyle: Partial<Styles> = {
+    control: (base: CSSProperties, props: any) => {
+        return {
+            ...base,
+            "borderRadius": 0,
+            "borderColor": props.isFocused ? "#A0AEC0" : "#CBD5E0",
+            "boxShadow": "none",
+            // @ts-ignore
+            "&:hover": {
+                borderColor: "#A0AEC0"
+            }
+        };
+    },
     option: (provided: CSSProperties, state: State<OptionTypeBase>): Partial<CSSProperties> => ({
         ...provided,
         color: "black"
