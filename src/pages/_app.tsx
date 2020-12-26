1007e9d1eef403a35bb1cc1ae913f7a833863b4a
Jared <jaredlll08@gmail.com>
2020-12-26 05:28:11 +0200

    Service worker for offline page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,19 +6,28 @@ import "../css/alerts.css";
 import "../css/nprogress.css";
 import "simplebar/dist/simplebar.min.css";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 import Router from "next/router";
 import NProgress from "nprogress";
 // @ts-ignore
 import { Provider } from "next-auth/client";
 
-NProgress.configure({showSpinner: false});
+NProgress.configure({ showSpinner: false });
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+                navigator.serviceWorker.register("/sw.js");
+            });
+        }
+    }, []);
+
     const { session } = pageProps;
     return (
         <Provider options={{ clientMaxAge: 10 * 60, keepAlive: 15 * 60 }} session={session}>
