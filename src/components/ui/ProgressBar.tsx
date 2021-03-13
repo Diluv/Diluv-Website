4136da7ab727d1e0a3b707772f0ec14a992c5187
Jared <jaredlll08@gmail.com>
2021-03-13 02:45:36 +0200

    progress bar looks a bit better

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const ProgressBar = ({ completed }: { completed: number }) => {
-
     return (
-        <div className={`bg-gray-200 dark:bg-dark-800 border-gray-300 dark:border-dark-700`}>
-            <div style={{ width: `${completed}%` }} className={`bg-diluv-500 text-black p-1 text-center`}>
-                <span >{`${Math.round(completed * 100) / 100 }%`}</span>
+        <div className={`relative bg-gray-200 dark:bg-dark-800 border-gray-300 dark:border-dark-700`}>
+            <div style={{ left: `calc(${completed}% - 1.5rem)`, top: "-2.5rem" }} className={`absolute align-top text-right`}>
+                <span className={`p-2 text-white bg-diluv-800`}>{`${Math.round(completed * 100) / 100}%`}</span>
             </div>
+            <div style={{ width: `${completed}%` }} className={`bg-diluv-500 h-8`} />
         </div>
     );
 };
