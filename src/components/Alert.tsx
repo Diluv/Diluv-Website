0b13f946e13dddf097875510c18c1f9f55448593
Jared <jaredlll08@gmail.com>
2021-06-07 04:57:09 +0200

    More code cleanup

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,11 +1,12 @@
 import React, { MutableRefObject, ReactNode, useRef, useState } from "react";
+import { XIcon } from "@heroicons/react/solid";
 
 function Alert(props: {
     className?: string;
     children?: ReactNode;
     type: "alert-danger" | "alert-warning" | "alert-success" | "alert-info";
     canDismiss?: boolean;
-    onDismiss?: () => void
+    onDismiss?: () => void;
 }): JSX.Element {
     const [closed, setClosed] = useState(false);
     const { canDismiss, type, children, className } = props;
@@ -19,7 +20,7 @@ function Alert(props: {
             {children}
             {canDismiss && (
                 <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg
+                    <XIcon
                         className={`fill-current h-6 w-6`}
                         onClick={() => {
                             const { current }: MutableRefObject<any> = alertRef;
@@ -33,17 +34,8 @@ function Alert(props: {
                                 }
                             }, 200);
                         }}
-                        role="button"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                    >
-                        <title>Close</title>
-                        <path
-                            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0
-              1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1
-              1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-                        />
-                    </svg>
+                        role={`button`}
+                    />
                 </span>
             )}
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,120 +6,115 @@ import ThemeSwitcher from "./ui/ThemeSwitcher";
 import { signin, signout, useSession } from "next-auth/client";
 import { getNameOrDefault } from "../utils/auth";
 import { SessionWithExtra } from "../interfaces";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
 
 function NavBar(): JSX.Element {
     const [showingMenu, setShowingMenu] = useState(false);
     const [showUserMenu, setShowingUserMenu] = useState(false);
-    const [session, loading] = useSession() as [SessionWithExtra | null, boolean];
+    const [session] = useSession() as [SessionWithExtra | null, boolean];
 
     return (
-        <>
-            <header className="text-gray-200 font-hero bg-gradient-to-br from-diluv-800 to-diluv-900">
-                <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row justify-between md:justify-start items-center">
-                    <div className={`w-full md:w-auto flex flex-row justify-between items-center`}>
-                        <Link href={"/"}>
-                            <a className="flex font-medium items-center text-white md:mb-0">
-                                <Drop className={`w-10 h-10`} />
-                                <span className="ml-3 text-xl">Diluv</span>
-                            </a>
-                        </Link>
-                        <button
-                            className={`border border-gray-500 hover:text-white hover:border-white p-1 block md:hidden`}
-                            onClick={() => setShowingMenu(!showingMenu)}
-                        >
-                            {showingMenu ? (
-                                <svg className={`fill-current w-5 h-5 block md:hidden`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-                                </svg>
-                            ) : (
-                                <svg className={`fill-current w-5 h-5 block md:hidden`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                                </svg>
-                            )}
-                        </button>
-                    </div>
-                    <div
-                        className={`w-full md:w-auto md:flex-grow flex flex-col md:flex-row justify-between md:inline-flex ${
-                            showingMenu ? `block` : `hidden`
-                        }`}
+        <header className="text-gray-200 font-hero bg-gradient-to-br from-diluv-800 to-diluv-900">
+            <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row justify-between md:justify-start items-center">
+                <div className={`w-full md:w-auto flex flex-row justify-between items-center`}>
+                    <Link href={"/"}>
+                        <a className="flex font-medium items-center text-white md:mb-0">
+                            <Drop className={`w-10 h-10`} />
+                            <span className="ml-3 text-xl">Diluv</span>
+                        </a>
+                    </Link>
+                    <button
+                        className={`border border-gray-500 hover:text-white hover:border-white p-1 block md:hidden`}
+                        onClick={() => setShowingMenu(!showingMenu)}
                     >
-                        <nav className="md:mr-auto md:ml-4 md:py-auto md:pl-4 md:border-l md:border-gray-700 flex flex-col md:flex-row flex-wrap items-center text-base text-center justify-center">
-                            <Link href={"/"}>
-                                <a className="md:mr-5 hover:text-white w-full md:w-auto block md:inline p-2 md:p-0">Home</a>
-                            </Link>
-                            <Link href={"/games"}>
-                                <a className="md:mr-5 hover:text-white w-full md:w-auto block md:inline p-2 md:p-0">Games</a>
-                            </Link>
-                            <Link href={"/docs/feedback"}>
-                                <a className="md:mr-5 hover:text-white w-full md:w-auto block md:inline p-2 md:p-0">Feedback</a>
-                            </Link>
-                        </nav>
-                        <div className="hidden md:flex gap-x-4">
-                            <DropDown name={getNameOrDefault(session, "Account")} className={`hover:text-white`}>
-                                {!session ? (
+                        {showingMenu ? (
+                            <XIcon className={`fill-current w-5 h-5 block md:hidden`} />
+                        ) : (
+                            <MenuIcon className={`fill-current w-5 h-5 block md:hidden`} />
+                        )}
+                    </button>
+                </div>
+                <div
+                    className={`w-full md:w-auto md:flex-grow flex flex-col md:flex-row justify-between md:inline-flex ${
+                        showingMenu ? `block` : `hidden`
+                    }`}
+                >
+                    <nav className="md:mr-auto md:ml-4 md:py-auto md:pl-4 md:border-l md:border-gray-700 flex flex-col md:flex-row flex-wrap items-center text-base text-center justify-center">
+                        <Link href={"/"}>
+                            <a className="nav-link">Home</a>
+                        </Link>
+                        <Link href={"/games"}>
+                            <a className="nav-link">Games</a>
+                        </Link>
+                        <Link href={"/docs/feedback"}>
+                            <a className="nav-link">Feedback</a>
+                        </Link>
+                    </nav>
+                    <div className="hidden md:flex gap-x-4">
+                        <DropDown name={getNameOrDefault(session, "Account")} className={`hover:text-white`}>
+                            {!session ? (
+                                <DropDownAction
+                                    action={async () => {
+                                        await signin("DILUV");
+                                    }}
+                                >
+                                    Sign in
+                                </DropDownAction>
+                            ) : (
+                                <>
+                                    <DropDownLinkInternal href={`/author/${session.user?.id}`}>Profile</DropDownLinkInternal>
                                     <DropDownAction
-                                        action={() => {
-                                            signin("DILUV");
+                                        action={async () => {
+                                            await signout();
+                                        }}
+                                    >
+                                        Sign out
+                                    </DropDownAction>
+                                </>
+                            )}
+                            <DropDownSpacer />
+                        </DropDown>
+                        <ThemeSwitcher />
+                    </div>
+                    <div className={`block md:hidden text-center`}>
+                        <p className={`hover:text-white cursor-pointer p-2 md:p-0`} onClick={() => setShowingUserMenu(!showUserMenu)}>
+                            {getNameOrDefault(session, "Account")}
+                        </p>
+                        <div className={`${showUserMenu ? `block` : `hidden`}`}>
+                            <div className={`flex flex-col`}>
+                                {!session ? (
+                                    <span
+                                        className={`hover:text-white p-2 md:p-0`}
+                                        onClick={async () => {
+                                            await signin("DILUV");
                                         }}
                                     >
                                         Sign in
-                                    </DropDownAction>
+                                    </span>
                                 ) : (
                                     <>
-                                        <DropDownLinkInternal href={`/author/${session.user?.id}`}>Profile</DropDownLinkInternal>
-                                        <DropDownAction
-                                            action={() => {
-                                                signout();
+                                        <Link href={`/author/${session.user?.id}`}>
+                                            <a>Profile</a>
+                                        </Link>
+                                        <span
+                                            className={`hover:text-white p-2 md:p-0`}
+                                            onClick={async () => {
+                                                await signout();
                                             }}
                                         >
                                             Sign out
-                                        </DropDownAction>
+                                        </span>
                                     </>
                                 )}
-                                <DropDownSpacer />
-                            </DropDown>
-                            <ThemeSwitcher />
+                            </div>
                         </div>
-                        <div className={`block md:hidden text-center`}>
-                            <p className={`hover:text-white cursor-pointer p-2 md:p-0`} onClick={() => setShowingUserMenu(!showUserMenu)}>
-                                {getNameOrDefault(session, "Account")}
-                            </p>
-                            <div className={`${showUserMenu ? `block` : `hidden`}`}>
-                                <div className={`flex flex-col`}>
-                                    {!session ? (
-                                        <span
-                                            className={`hover:text-white p-2 md:p-0`}
-                                            onClick={(event) => {
-                                                signin("DILUV");
-                                            }}
-                                        >
-                                            Sign in
-                                        </span>
-                                    ) : (
-                                        <>
-                                            <Link href={`/author/${session.user?.id}`}>
-                                                <a>Profile</a>
-                                            </Link>
-                                            <span
-                                                className={`hover:text-white p-2 md:p-0`}
-                                                onClick={() => {
-                                                    signout();
-                                                }}
-                                            >
-                                                Sign out
-                                            </span>
-                                        </>
-                                    )}
-                                </div>
-                            </div>
-                            <div className={`mx-auto p-2`}>
-                                <ThemeSwitcher />
-                            </div>
+                        <div className={`mx-auto p-2`}>
+                            <ThemeSwitcher />
                         </div>
                     </div>
                 </div>
-            </header>
-        </>
+            </div>
+        </header>
     );
 }
 
